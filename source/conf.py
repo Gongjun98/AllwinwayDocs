e2193c58309cdb88957fae23bf84506d0d8864ae
rainti <58545686@qq.comm>
2025-07-31 09:59:05 +0800

    refactor(improve structures): add external link

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ if on_rtd:
     #git_lfs.fetch_urls = _patched_fetch_urls
     #git_lfs.fetch(PROJECT_ROOT_DIR)
     if not os.path.exists('./git-lfs/git-lfs'):
-      print("ÉèÖÃGit LFS...")
+      print("StartGit LFS...")
       os.makedirs('./git-lfs', exist_ok=True)
       os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v3.4.0/git-lfs-linux-amd64-v3.4.0.tar.gz')
       os.system('tar xzf git-lfs-linux-amd64-v3.4.0.tar.gz -C ./git-lfs --strip-components=1')
@@ -57,7 +57,7 @@ if on_rtd:
       os.system('./git-lfs/git-lfs fetch')
       os.system('./git-lfs/git-lfs checkout')
       os.system('rm git-lfs-linux-amd64-v3.4.0.tar.gz')
-      print("Git LFS ÉèÖÃÍê³É")
+      print("Git LFS END")
 
 else:
     render_examples = True
@@ -67,6 +67,7 @@ else:
 extensions = [
     'myst_parser',
     'sphinx_markdown_tables'
+    'sphinx.ext.intersphinx',
     #"sphinxcontrib.mermaid",
     #"sphinx_copybutton",
 ]
@@ -97,6 +98,16 @@ html_theme = "sphinx_rtd_theme"
 #html_static_path = ['_static']
 html_extra_path = ['.']  # È·±£¸ùÄ¿Â¼±»°üº¬
 html_search_language = 'zh'
+
+# 配置其他RTD项目的映射
+intersphinx_mapping = {
+    'allwinner2': ('https://allwinwaydocs2.readthedocs.io/zh-cn/latest/', None),
+    #'hardware': ('https://hardware-project.readthedocs.io/en/latest/', None),
+    #'software': ('https://software-guide.readthedocs.io/en/latest/', None),
+}
+
+# 超时设置（RTD免费版网络可能较慢）
+intersphinx_timeout = 10
 
 print(sys.executable)
 
